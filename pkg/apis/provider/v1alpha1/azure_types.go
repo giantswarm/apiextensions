75c93359f047754a11e598787fb09d20b99745ca
Paweł Kopiczko <kopiczko@gmail.com>
2018-01-11 15:08:38 +0100

    provider/v1alpha1: remove Azure node SSH config (#42)
    In the operator it is reused from Cluster part of the object.
    See: https://github.com/giantswarm/azure-operator/pull/97.

--- pkg/apis/provider/v1alpha1/azure_types.go
+++ pkg/apis/provider/v1alpha1/azure_types.go
@@ -107,10 +107,6 @@ type AzureConfigSpecAzureVirtualNetwork struct {
 }
 
 type AzureConfigSpecAzureNode struct {
-	// AdminUsername is the vm administrator username
-	AdminUsername string `json:"adminUsername" yaml:"adminUsername"`
-	//  AdminSSHKeyData is the vm administrator ssh public key
-	AdminSSHKeyData string `json:"adminSSHKeyData" yaml:"adminSSHKeyData"`
 	// VMSize is the master vm size (e.g. Standard_A1)
 	VMSize string `json:"vmSize" yaml:"vmSize"`
 }
