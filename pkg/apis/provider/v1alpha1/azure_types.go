da7b03c10297c34b5e72e4b89189d4c8ce52fade
Theo Brigitte <theo.brigitte@gmail.com>
2018-07-02 14:16:36 +0200

    update comment

--- pkg/apis/provider/v1alpha1/azure_types.go
+++ pkg/apis/provider/v1alpha1/azure_types.go
@@ -96,7 +96,7 @@ type AzureConfigSpecAzureVirtualNetwork struct {
 	// CIDR is the CIDR for the Virtual Network.
 	CIDR string `json:"cidr" yaml:"cidr"`
 
-	// TODO: remove Master, Worker and Calico subnet cidr after v2 is deleted.
+	// TODO: remove Master, Worker and Calico subnet cidr after azure-operator v2 is deleted.
 	// MasterSubnetCIDR is the CIDR for the master subnet.
 	MasterSubnetCIDR string `json:"masterSubnetCIDR" yaml:"masterSubnetCIDR"`
 	// WorkerSubnetCIDR is the CIDR for the worker subnet.
