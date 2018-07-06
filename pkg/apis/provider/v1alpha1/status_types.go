55e9153bca9ffd9d36683bcb5769576731614160
Tim Schindler <xh3b4sd@gmail.com>
2018-07-07 00:24:01 +0200

    adds nodes to cluster status

--- pkg/apis/provider/v1alpha1/status_types.go
+++ pkg/apis/provider/v1alpha1/status_types.go
@@ -18,8 +18,17 @@ const (
 )
 
 type StatusCluster struct {
+	// Conditions is a list of status information expressing the current
+	// conditional state of a guest cluster. This may reflect the status of the
+	// guest cluster being updating or being up to date.
 	Conditions []StatusClusterCondition `json:"conditions" yaml:"conditions"`
-	Versions   []StatusClusterVersion   `json:"versions" yaml:"versions"`
+	// Nodes is a list of guest cluster node information reflecting the current
+	// state of the guest cluster nodes.
+	Nodes []StatusClusterNode `json:"nodes" yaml:"nodes"`
+	// Versions is a list that acts like a historical track record of versions a
+	// guest cluster went through. A version is only added to the list as soon as
+	// the guest cluster successfully migrated to the version added here.
+	Versions []StatusClusterVersion `json:"versions" yaml:"versions"`
 }
 
 // StatusClusterCondition expresses the conditions in which a guest cluster may
@@ -32,8 +41,14 @@ type StatusClusterCondition struct {
 	Type string `json:"type" yaml:"type"`
 }
 
+// StatusClusterNode holds information about a guest cluster node.
+type StatusClusterNode struct {
+	Name    string `json:"name" yaml:"name"`
+	Version string `json:"version" yaml:"version"`
+}
+
 // StatusClusterVersion expresses the versions in which a guest cluster was and
-// is.
+// may still is.
 type StatusClusterVersion struct {
 	// Date is the time of the given guest cluster version being updated.
 	Date time.Time `json:"date" yaml:"date"`
