9102b9289496f332e03337a707e57ca7e8d59a12
Ross Fairbanks <ross@giantswarm.io>
2019-01-23 14:23:54 +0100

    Add AppCatalog CR example

--- pkg/apis/application/v1alpha1/app_catalog_types.go
+++ pkg/apis/application/v1alpha1/app_catalog_types.go
@@ -43,6 +43,30 @@ func NewAppCatalogCRD() *apiextensionsv1beta1.CustomResourceDefinition {
 	}
 }
 
+// AppCatalog CRs might look something like the following.
+//
+// apiVersion: application.giantswarm.io/v1alpha1
+// kind: AppCatalog
+// metadata:
+//   name: “giantswarm”
+//   labels:
+//     app-operator.giantswarm.io/version: "1.0.0"
+//
+// spec:
+//   title: “Giant Swarm”
+//   description: “Catalog of Apps by Giant Swarm”
+//   catalogStorage:
+//     type: "helm"
+//     URL: “https://giantswarm.github.com/app-catalog/”
+//   config:
+//     configMap:
+//       name: "app-catalog-values"
+//       namespace: "giantswarm"
+//    secret:
+//      name: "app-catalog-secrets"
+//      namespace: "giantswarm"
+//  logoURL: “https://s.giantswarm.io/...”
+
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
