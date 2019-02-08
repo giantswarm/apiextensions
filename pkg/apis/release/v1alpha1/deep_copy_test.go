b42b15622a6d62236ec4b374373eb826179dcc1c
Paweł Kopiczko <kopiczko@gmail.com>
2019-02-08 14:23:08 +0000

    test the actual parsing

--- pkg/apis/release/v1alpha1/deep_copy_test.go
+++ pkg/apis/release/v1alpha1/deep_copy_test.go
@@ -2,33 +2,46 @@ package v1alpha1
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func Test_DeepCopyDate_UnmarshalJSON(t *testing.T) {
 	testCases := []struct {
-		name         string
-		json         string
-		errorMatcher func(err error) bool
+		name          string
+		inputJSON     string
+		expectedDay   int
+		expectedMonth time.Month
+		expectedYear  int
+		errorMatcher  func(err error) bool
 	}{
 		{
-			name:         "case 0: valid date",
-			json:         `{"testDate": "2019-02-08"}`,
-			errorMatcher: nil,
+			name:          "case 0: valid date",
+			inputJSON:     `{"testDate": "2019-02-08"}`,
+			expectedDay:   8,
+			expectedMonth: 2,
+			expectedYear:  2019,
+			errorMatcher:  nil,
 		},
 		{
 			name:         "case 1: malformed date",
-			json:         `{"testDate": "2019-02-08T12:04:00"}`,
+			inputJSON:    `{"testDate": "2019-02-08T12:04:00"}`,
 			errorMatcher: func(err error) bool { return err != nil },
 		},
 		{
-			name:         "case 2: null",
-			json:         `{"testDate": null}`,
-			errorMatcher: nil,
+			name:          "case 2: null",
+			inputJSON:     `{"testDate": null}`,
+			expectedDay:   1,
+			expectedMonth: 1,
+			expectedYear:  1,
+			errorMatcher:  nil,
 		},
 		{
 			name:         "case 3: wrong type",
-			json:         `{"testDate": 5}`,
+			inputJSON:    `{"testDate": 5}`,
 			errorMatcher: func(err error) bool { return err != nil },
 		},
 	}
@@ -40,7 +53,7 @@ func Test_DeepCopyDate_UnmarshalJSON(t *testing.T) {
 			}
 
 			var jsonObject JSONObject
-			err := json.Unmarshal([]byte(tc.json), &jsonObject)
+			err := json.Unmarshal([]byte(tc.inputJSON), &jsonObject)
 
 			switch {
 			case err == nil && tc.errorMatcher == nil:
@@ -55,6 +68,16 @@ func Test_DeepCopyDate_UnmarshalJSON(t *testing.T) {
 
 			if tc.errorMatcher != nil {
 				return
+			}
+
+			if !reflect.DeepEqual(jsonObject.TestDate.Day(), tc.expectedDay) {
+				t.Errorf("\n\n%s\n", cmp.Diff(jsonObject.TestDate.Day(), tc.expectedDay))
+			}
+			if !reflect.DeepEqual(jsonObject.TestDate.Month(), tc.expectedMonth) {
+				t.Errorf("\n\n%s\n", cmp.Diff(jsonObject.TestDate.Month(), tc.expectedMonth))
+			}
+			if !reflect.DeepEqual(jsonObject.TestDate.Year(), tc.expectedYear) {
+				t.Errorf("\n\n%s\n", cmp.Diff(jsonObject.TestDate.Year(), tc.expectedYear))
 			}
 		})
 	}
