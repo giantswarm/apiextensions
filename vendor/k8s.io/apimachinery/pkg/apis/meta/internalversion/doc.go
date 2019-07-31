1f77311451f3aa3c60b563a5151c9ca881f78069
jihyuk <jhbok@giantswarm.io>
2019-07-31 14:18:36 +0200

    add missing vendor

--- vendor/k8s.io/apimachinery/pkg/apis/meta/internalversion/doc.go
+++ vendor/k8s.io/apimachinery/pkg/apis/meta/internalversion/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2016 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,9 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package scheme contains utilities for gradually building Schemes,
-// which contain information associating Go types with Kubernetes
-// groups, versions, and kinds.
-//
-// Deprecated: use pkg/scheme instead.
-package scheme
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=k8s.io/apimachinery/pkg/apis/meta/v1
+
+package internalversion
