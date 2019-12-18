df3b014d0dcf74ca7998093e553e3933748da672
Marian Steinbach <marian@giantswarm.io>
2019-12-18 11:08:26 +0100

    Remove genclient:noStatus

--- pkg/apis/example/v1alpha1/githubsearchindex_types.go
+++ pkg/apis/example/v1alpha1/githubsearchindex_types.go
@@ -60,7 +60,6 @@ func NewGithubSearchIndexCRD() *apiextensionsv1beta1.CustomResourceDefinition {
 }
 
 // +genclient
-// +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 type GitHubSearchIndex struct {
