ba943d6ec7816daebde5c8aaebd3c54cd0696297
Thomas Fussell <thomas@fussell.io>
2020-01-28 11:42:49 -0500

    save work

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -5,6 +5,10 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "$(go list -m -f '{{.Dir}}' k8s.io/code-generator)/generate-groups.sh"  \
     "deepcopy,client" \
     github.com/giantswarm/apiextensions/pkg \
-    ./pkg/apis \
+    github.com/giantswarm/apiextensions/pkg/apis \
     "application:v1alpha1 core:v1alpha1 example:v1alpha1 provider:v1alpha1 release:v1alpha1 infrastructure:v1alpha2" \
+    --output-base "$dir" \
     --go-header-file "${dir}/boilerplate.go.txt"
+
+cp -R "$dir/github.com/giantswarm/apiextensions/pkg"/* "$dir/../pkg"
+rm -rf "$dir/github.com"
