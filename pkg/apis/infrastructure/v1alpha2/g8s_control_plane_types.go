5319eb0e1fc4459302eb8bd1caefa32a714ff3cb
Thomas Fussell <thomas@giantswarm.io>
2020-02-08 15:39:55 -0500

    use k8s.io/api instead of k8s.io/kubernetes

--- pkg/apis/infrastructure/v1alpha2/g8s_control_plane_types.go
+++ pkg/apis/infrastructure/v1alpha2/g8s_control_plane_types.go
@@ -4,7 +4,7 @@ import (
 	"github.com/ghodss/yaml"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	corev1 "k8s.io/kubernetes/pkg/apis/core"
+	corev1 "k8s.io/api/core/v1"
 )
 
 const (
