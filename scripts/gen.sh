8bcceb0ab1b8519f1a9fe8d25b1a4fe7b11cac11
Thomas Fussell <thomas@giantswarm.io>
2020-03-03 08:05:53 -0500

    Use architect orb (#343)
    * use architect orb
    
    * dep ensure
    
    * order imports
    
    * add goimports to gen.sh

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -8,3 +8,13 @@ cd ${dir}/../vendor/k8s.io/code-generator && ./generate-groups.sh \
     github.com/giantswarm/apiextensions/pkg/apis \
     "application:v1alpha1 core:v1alpha1 example:v1alpha1 provider:v1alpha1 release:v1alpha1 infrastructure:v1alpha2" \
     --go-header-file ${dir}/boilerplate.go.txt
+
+# code-generator doesn't group local imports separately from third-party
+# imports, so run goimports after generating new code.
+cd "$dir/.." || exit
+if [ ! -x "$(command -v goimports)" ]; then
+  echo "goimports not found, downloading to $GOPATH/bin using 'go get'"
+  go get golang.org/x/tools/cmd/goimports
+fi
+echo "Fixing imports in-place with goimports"
+"$GOPATH"/bin/goimports -local github.com/giantswarm/apiextensions -w ./pkg
