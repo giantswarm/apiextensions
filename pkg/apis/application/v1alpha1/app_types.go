7336af47294fe9ad451ac8d585d6ceaccccfe00e
Marian Steinbach <marian@giantswarm.io>
2020-03-09 13:18:51 +0100

    Add property descriptions

--- pkg/apis/application/v1alpha1/app_types.go
+++ pkg/apis/application/v1alpha1/app_types.go
@@ -29,72 +29,127 @@ spec:
     status: {}
   validation:
     openAPIV3Schema:
+      description: |
+        Defines an App resource, which represents an application to be running in a Kubernetes cluster.
+        Reconciled by app-operator.
       properties:
         spec:
           type: object
           properties:
             catalog:
+              description: |
+                Name of the AppCatalog to install this app from. Find more information in the AppCatalog
+                CRD documentation.
               type: string
             name:
+              description: |
+                Name of this App.
               type: string
             namespace:
+              description: |
+                Kubernetes namespace in which to install the workloads defined by this App.
               type: string
             version:
+              description: Version of the app to be deployed.
               type: string
             config:
+              description: |
+                Configuration details for the app.
               type: object
               properties:
                 configMap:
+                  description: |
+                    If present, points to a ConfigMap resource that holds configuration data
+                    used by the app.
                   type: object
                   properties:
                     name:
+                      description: |
+                        Name of the ConfigMap.
                       type: string
                     namespace:
+                      description: |
+                        Namespace to find the ConfigMap in.
                       type: string
                   required: ["name", "namespace"]
                 secret:
+                  description: |
+                    If present, points to a Secret resoure that can be used by the app.
                   type: object
                   properties:
                     name:
+                      description: |
+                        Name of the Secret.
                       type: string
                     namespace:
+                      description: |
+                        Namespace to find the Secret in.
                       type: string
                   required: ["name", "namespace"]
             kubeConfig:
+              description: |
+                The kubeconfig to use to connect to the tenant cluster when deploying the app.
               type: object
               properties:
                 inCluster:
+                  description: |
+                    Defines whether to use inCluster credentials. If true, the context and secret
+                    properties must not be set.
                   type: boolean
                 context:
+                  description: |
+                    Kubeconfig context part to use when not using inCluster credentials.
                   type: object
                   properties:
                     name:
+                      description: |
+                        Context name.
                       type: string
                 secret:
+                  description: |
+                    References a Secret resource holding the kubeconfig details, if not using inCluster credentials.
                   type: object
                   properties:
                     name:
+                      description: |
+                        Name of the Secret resource.
                       type: string
                     namespace:
+                      description: |
+                        Namespace holding the Secret resource.
                       type: string
                   required: ["name", "namespace"]
             userConfig:
+              description: |
+                Additional and optional user-provided configuration for the app.
               type: object
               properties:
                 configMap:
+                  description: |
+                    Reference to an optional ConfigMap.
                   type: object
                   properties:
                     name:
+                      description: |
+                        Name of the ConfigMap resource.
                       type: string
                     namespace:
+                      description: |
+                        Namespace holding the ConfigMap resource.
                       type: string
                   required: ["name", "namespace"]
                 secret:
+                  description: |
+                    Reference to an optional Secret resource.
                   type: object
                   properties:
                     name:
+                      description: |
+                        Name of the Secret resource.
                       type: string
                     namespace:
+                      description: |
+                        Namespace holding the Secret resource.
                       type: string
                   required: ["name", "namespace"]
           required: ["catalog", "name", "namespace", "version"]
