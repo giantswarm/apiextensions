e9145c310f00934b04f7e42e6af46f1daaae3d43
Thomas Fussell <thomas@giantswarm.io>
2020-03-16 09:58:00 -0400

    clean up

--- pkg/apis/release/v1alpha1/release_types_test.go
+++ pkg/apis/release/v1alpha1/release_types_test.go
@@ -6,6 +6,7 @@ func Test_NewReleaseCRD(t *testing.T) {
 	crd := NewReleaseCRD()
 	if crd == nil {
 		t.Error("Release CRD was nil.")
+		return
 	}
 	if crd.Name == "" {
 		t.Error("Release CRD name was empty.")
