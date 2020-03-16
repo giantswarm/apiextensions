2c8cdd380355ea9d7fd3c14941f9ba1750cf21e2
Ross Fairbanks <ross@rossfairbanks.com>
2020-03-16 12:42:02 +0100

    Fix go vet error with chart DeepCopyTime

--- pkg/apis/application/v1alpha1/deep_copy.go
+++ pkg/apis/application/v1alpha1/deep_copy.go
@@ -7,7 +7,7 @@ import (
 // DeepCopyTime implements the deep copy logic for time.Time which the k8s
 // codegen is not able to generate out of the box.
 type DeepCopyTime struct {
-	time.Time
+	DeepCopyTime time.Time
 }
 
 func (in *DeepCopyTime) DeepCopyInto(out *DeepCopyTime) {
