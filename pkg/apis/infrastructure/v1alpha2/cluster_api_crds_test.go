579dc36b7a151cee648a19e49f6d2a3d434e4aa6
Marian Steinbach <marian@giantswarm.io>
2020-04-03 17:52:33 +0200

    Document MachineDeployment CRD (#381)
    * Initialize MachineDeployment CRD from YAML string
    
    * Create MachineDeployment CR and CRD via unit test

--- pkg/apis/infrastructure/v1alpha2/cluster_api_crds_test.go
+++ pkg/apis/infrastructure/v1alpha2/cluster_api_crds_test.go
@@ -95,3 +95,93 @@ func newClusterExampleCR() *apiv1alpha2.Cluster {
 
 	return cr
 }
+
+func Test_NewMachineDeploymentCRD(t *testing.T) {
+	crd := NewMachineDeploymentCRD()
+	if crd == nil {
+		t.Error("MachineDeployment CRD was nil.")
+	}
+	if crd.Name == "" {
+		t.Error("MachineDeployment CRD name was empty.")
+	}
+}
+
+func Test_GenerateMachineDeploymentYAML(t *testing.T) {
+	testCases := []struct {
+		category string
+		name     string
+		resource runtime.Object
+	}{
+		{
+			category: "crd",
+			name:     fmt.Sprintf("%s_machinedeployment.yaml", group),
+			resource: NewMachineDeploymentCRD(),
+		},
+		{
+			category: "cr",
+			name:     fmt.Sprintf("%s_%s_machinedeployment.yaml", group, version),
+			resource: newMachineDeploymentExampleCR(),
+		},
+	}
+
+	docs := filepath.Join(root, "..", "..", "..", "..", "docs")
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("case %d: generates %s successfully", i, tc.name), func(t *testing.T) {
+			rendered, err := yaml.Marshal(tc.resource)
+			if err != nil {
+				t.Fatal(err)
+			}
+			directory := filepath.Join(docs, tc.category)
+			path := filepath.Join(directory, tc.name)
+
+			// We don't want a status in the docs YAML for the CR and CRD so that they work with `kubectl create -f <file>.yaml`.
+			// This just strips off the top level `status:` and everything following.
+			statusRegex := regexp.MustCompile(`(?ms)^status:.*$`)
+			rendered = statusRegex.ReplaceAll(rendered, []byte(""))
+
+			if *update {
+				err := ioutil.WriteFile(path, rendered, 0644)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+			goldenFile, err := ioutil.ReadFile(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !bytes.Equal(rendered, goldenFile) {
+				t.Fatalf("\n\n%s\n", cmp.Diff(string(goldenFile), string(rendered)))
+			}
+		})
+	}
+}
+
+func newMachineDeploymentExampleCR() *apiv1alpha2.MachineDeployment {
+	cr := NewMachineDeploymentCR()
+
+	ten := int32(10)
+
+	cr.Name = "e3z8q"
+	cr.Spec = apiv1alpha2.MachineDeploymentSpec{
+		Replicas: &ten,
+		Template: apiv1alpha2.MachineTemplateSpec{
+			Spec: apiv1alpha2.MachineSpec{
+				InfrastructureRef: corev1.ObjectReference{
+					APIVersion:      "infrastructure.giantswarm.io/v1alpha2",
+					Kind:            "AWSMachineDeployment",
+					Name:            "e3z8q",
+					Namespace:       "default",
+					ResourceVersion: "58776588",
+					UID:             "b5fdf153-6f10-462b-a49f-0e821873d16b",
+				},
+			},
+		},
+	}
+	cr.Status = apiv1alpha2.MachineDeploymentStatus{
+		ReadyReplicas: 8,
+		Replicas:      10,
+	}
+
+	return cr
+}
