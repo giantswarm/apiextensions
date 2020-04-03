3257d94ce6ebcd87f283efc3587035e84cadb518
Marian Steinbach <marian@giantswarm.io>
2020-04-03 17:57:46 +0200

    Document Cluster CRD (#375)
    * Create Cluster CRD based on YAML string
    
    * Create Cluster CR YAML
    
    * Make example CR more realistic
    
    * Rename import alias
    
    * Use current godoc URL as documentation link
    
    * Update generated file
    
    * Revert dependency updates
    
    * Document MachineDeployment CRD (#381)
    
    * Initialize MachineDeployment CRD from YAML string
    
    * Create MachineDeployment CR and CRD via unit test

--- go.mod
+++ go.mod
@@ -4,13 +4,14 @@ go 1.13
 
 require (
 	github.com/go-openapi/errors v0.19.4
-	github.com/google/go-cmp v0.3.0
+	github.com/google/go-cmp v0.4.0
 	k8s.io/api v0.16.6
 	k8s.io/apiextensions-apiserver v0.16.6
 	k8s.io/apimachinery v0.16.6
 	k8s.io/client-go v0.16.6
 	k8s.io/code-generator v0.16.6
-	sigs.k8s.io/yaml v1.1.0
+	sigs.k8s.io/cluster-api v0.2.10
+	sigs.k8s.io/yaml v1.2.0
 )
 
 replace golang.org/x/tools => golang.org/x/tools v0.0.0-20190821162956-65e3620a7ae7
--- pkg/apis/infrastructure/v1alpha2/cluster_api_crds.go
+++ pkg/apis/infrastructure/v1alpha2/cluster_api_crds.go
@@ -1,9 +1,91 @@
 package v1alpha2
 
 import (
+	"fmt"
+
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiv1alpha2 "sigs.k8s.io/cluster-api/api/v1alpha2"
+	"sigs.k8s.io/yaml"
 )
+
+const (
+	kindCluster                        = "Cluster"
+	kindMachineDeployment              = "MachineDeployment"
+	clusterDocumentationLink           = "https://pkg.go.dev/sigs.k8s.io/cluster-api/api/v1alpha2?tab=doc#Cluster"
+	machineDeploymentDocumentationLink = "https://pkg.go.dev/sigs.k8s.io/cluster-api/api/v1alpha2?tab=doc#MachineDeployment"
+)
+
+const clusterCRDYAML = `
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
+metadata:
+  labels:
+    controller-tools.k8s.io: "1.0"
+  name: clusters.cluster.x-k8s.io
+spec:
+  conversion:
+    strategy: None
+  group: cluster.x-k8s.io
+  names:
+    kind: Cluster
+    listKind: ClusterList
+    plural: clusters
+    singular: cluster
+  preserveUnknownFields: true
+  scope: Namespaced
+  versions:
+  - name: v1alpha2
+    served: true
+    storage: true
+    subresources:
+      status: {}
+`
+
+const machineDeploymentCRDYAML = `
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
+metadata:
+  labels:
+    controller-tools.k8s.io: "1.0"
+  name: machinedeployments.cluster.x-k8s.io
+spec:
+  conversion:
+    strategy: None
+  group: cluster.x-k8s.io
+  names:
+    kind: MachineDeployment
+    listKind: MachineDeploymentList
+    plural: machinedeployments
+    singular: machinedeployment
+  preserveUnknownFields: true
+  scope: Namespaced
+  versions:
+  - name: v1alpha2
+    served: true
+    storage: true
+    subresources:
+      status: {}
+`
+
+var (
+	clusterCRD           *apiextensionsv1beta1.CustomResourceDefinition
+	machineDeploymentCRD *apiextensionsv1beta1.CustomResourceDefinition
+)
+
+func init() {
+	err := yaml.Unmarshal([]byte(clusterCRDYAML), &clusterCRD)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	err = yaml.Unmarshal([]byte(machineDeploymentCRDYAML), &machineDeploymentCRD)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
 
 // NewClusterCRD returns a new custom resource definition for Cluster (from
 // Cluster API). This might look something like the following.
@@ -24,29 +106,25 @@ import (
 //         status: {}
 //
 func NewClusterCRD() *apiextensionsv1beta1.CustomResourceDefinition {
-	return &apiextensionsv1beta1.CustomResourceDefinition{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: apiextensionsv1beta1.SchemeGroupVersion.String(),
-			Kind:       "CustomResourceDefinition",
-		},
+	return clusterCRD.DeepCopy()
+}
+
+func NewClusterTypeMeta() metav1.TypeMeta {
+	return metav1.TypeMeta{
+		APIVersion: SchemeGroupVersion.String(),
+		Kind:       kindCluster,
+	}
+}
+
+// NewClusterCR returns a Cluster Custom Resource.
+func NewClusterCR() *apiv1alpha2.Cluster {
+	return &apiv1alpha2.Cluster{
 		ObjectMeta: metav1.ObjectMeta{
-			Labels: map[string]string{
-				"controller-tools.k8s.io": "1.0",
+			Annotations: map[string]string{
+				crDocsAnnotation: clusterDocumentationLink,
 			},
-			Name: "clusters.cluster.x-k8s.io",
 		},
-		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group: "cluster.x-k8s.io",
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-				Kind:   "Cluster",
-				Plural: "clusters",
-			},
-			Scope: apiextensionsv1beta1.NamespaceScoped,
-			Subresources: &apiextensionsv1beta1.CustomResourceSubresources{
-				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
-			},
-			Version: "v1alpha2",
-		},
+		TypeMeta: NewClusterTypeMeta(),
 	}
 }
 
@@ -70,28 +148,25 @@ func NewClusterCRD() *apiextensionsv1beta1.CustomResourceDefinition {
 //         status: {}
 //
 func NewMachineDeploymentCRD() *apiextensionsv1beta1.CustomResourceDefinition {
-	return &apiextensionsv1beta1.CustomResourceDefinition{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: apiextensionsv1beta1.SchemeGroupVersion.String(),
-			Kind:       "CustomResourceDefinition",
-		},
+	return machineDeploymentCRD.DeepCopy()
+}
+
+// NewMachineDeploymentTypeMeta returns the type block for a MachineDeployment CR.
+func NewMachineDeploymentTypeMeta() metav1.TypeMeta {
+	return metav1.TypeMeta{
+		APIVersion: SchemeGroupVersion.String(),
+		Kind:       kindMachineDeployment,
+	}
+}
+
+// NewMachineDeploymentCR returns a MachineDeployment Custom Resource.
+func NewMachineDeploymentCR() *apiv1alpha2.MachineDeployment {
+	return &apiv1alpha2.MachineDeployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Labels: map[string]string{
-				"controller-tools.k8s.io": "1.0",
+			Annotations: map[string]string{
+				crDocsAnnotation: machineDeploymentDocumentationLink,
 			},
-			Name: "machinedeployments.cluster.x-k8s.io",
 		},
-		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group: "cluster.x-k8s.io",
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-				Kind:   "MachineDeployment",
-				Plural: "machinedeployments",
-			},
-			Scope: apiextensionsv1beta1.NamespaceScoped,
-			Subresources: &apiextensionsv1beta1.CustomResourceSubresources{
-				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
-			},
-			Version: "v1alpha2",
-		},
+		TypeMeta: NewMachineDeploymentTypeMeta(),
 	}
 }
