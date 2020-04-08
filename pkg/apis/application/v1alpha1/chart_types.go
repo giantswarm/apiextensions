851b8707a8e8d7dca6f3728e4333b1dcecbd8197
Ross Fairbanks <ross@rossfairbanks.com>
2020-04-08 15:47:14 +0200

    Add revision to chart CR status

--- pkg/apis/application/v1alpha1/chart_types.go
+++ pkg/apis/application/v1alpha1/chart_types.go
@@ -215,6 +215,8 @@ type ChartStatus struct {
 type ChartStatusRelease struct {
 	// LastDeployed is the time when the deployed chart was last deployed.
 	LastDeployed DeepCopyTime `json:"lastDeployed" yaml:"lastDeployed"`
+	// Revision is the revision number for this deployed chart.
+	Revision int `json:"revision" yaml:"revision"`
 	// Status is the status of the deployed chart,
 	// e.g. DEPLOYED.
 	Status string `json:"status" yaml:"status"`
