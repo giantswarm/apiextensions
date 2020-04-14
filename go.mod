d7f7c70e805a1de9ace86be15dd438598891f997
Thomas Fussell <thomas@giantswarm.io>
2020-04-14 14:48:05 -0400

    switch crds to v1, clean up gen.sh, separate scripts module

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/giantswarm/apiextensions
 
-go 1.13
+go 1.14
 
 require (
 	github.com/go-openapi/errors v0.19.4
@@ -9,10 +9,6 @@ require (
 	k8s.io/apiextensions-apiserver v0.16.6
 	k8s.io/apimachinery v0.16.6
 	k8s.io/client-go v0.16.6
-	k8s.io/code-generator v0.16.6
 	sigs.k8s.io/cluster-api v0.2.10
-	sigs.k8s.io/controller-tools v0.2.4
 	sigs.k8s.io/yaml v1.2.0
 )
-
-replace golang.org/x/tools => golang.org/x/tools v0.0.0-20190821162956-65e3620a7ae7
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,30 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export GOPATH=$(go env GOPATH) # ensure that GOPATH is defined for generate-groups.sh
-go mod tidy # ensure that code-generator is cached in $GOPATH/pkg/mod for generate-groups.sh
+export GOPATH=$(go env GOPATH)
+export GOROOT=$(go env GOROOT)
 
-bash "$(go list -m -f '{{.Dir}}' k8s.io/code-generator)/generate-groups.sh"  \
-    "deepcopy,client" \
-    github.com/giantswarm/apiextensions/pkg \
-    github.com/giantswarm/apiextensions/pkg/apis \
-    "application:v1alpha1 core:v1alpha1 example:v1alpha1 provider:v1alpha1 release:v1alpha1 infrastructure:v1alpha2 backup:v1alpha1" \
-    --output-base "$dir" \
-    --go-header-file "${dir}/boilerplate.go.txt"
+tools="sigs.k8s.io/controller-tools/cmd/controller-gen
+k8s.io/code-generator/cmd/deepcopy-gen
+k8s.io/code-generator/cmd/client-gen
+golang.org/x/tools/cmd/goimports"
 
-cp -R "$dir/github.com/giantswarm/apiextensions/pkg"/* "$dir/../pkg"
+mkdir -p "$dir/bin"
+for tool in $tools; do
+  go build -o "$dir/bin/$(basename "$tool")" "$tool"
+done
+
+exit 0
+module="github.com/giantswarm/apiextensions"
+input_dirs=$(find ./pkg/apis -maxdepth 2 -mindepth 2 | tr '\r\n' ',')
+input_dirs=${input_dirs%?}
+groups=${input_dirs//.\/pkg\/apis\//}
+header="${dir}/boilerplate.go.txt"
+
+echo "Generating deepcopy funcs"
+"$dir/bin/deepcopy-gen" \
+  --input-dirs "$input_dirs" \
+  --output-file-base zz_generated.deepcopy \
+  --output-base "$dir/.." \
+  --go-header-file "${dir}/boilerplate.go.txt"
+
+echo "Generating clientset"
+"$dir/bin/client-gen" \
+  --clientset-name versioned \
+  --input "$groups" \
+  --input-base "$module/pkg/apis" \
+  --output-package "$module/pkg/clientset" \
+  --output-base "$dir" \
+  --go-header-file "$header"
+
+cp -R "$dir/$module/pkg"/* "$dir/../pkg"
 rm -rf "$dir/github.com"
 
 # code-generator doesn't group local imports separately from third-party
 # imports, so run goimports after generating new code.
 cd "$dir/.." || exit
-if [ ! -x "$(command -v goimports)" ]; then
-  echo "goimports not found, downloading to $GOPATH/bin using 'go get'"
-  go get golang.org/x/tools/cmd/goimports
-  export PATH=$PATH:$GOPATH/bin
-fi
 echo "Fixing imports in-place with goimports"
-goimports -local github.com/giantswarm/apiextensions -w ./pkg
+"$dir/bin/goimports" -local $module -w ./pkg
 
-go run "$(go list -m -f '{{.Dir}}' sigs.k8s.io/controller-tools)"/cmd/controller-gen/main.go  \
-    crd paths=./pkg/apis/... output:dir=docs/crd
+"$dir/bin/controller-gen" \
+  crd \
+  paths=./pkg/apis/... \
+  output:dir=docs/crd \
+  crd:crdVersions=v1
