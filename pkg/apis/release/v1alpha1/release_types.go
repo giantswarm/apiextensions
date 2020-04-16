8651cfd947d1a116b8af6dc126a4d980862586df
Thomas Fussell <thomas@giantswarm.io>
2020-04-15 23:08:59 -0400

    ignore lines entirely

--- pkg/apis/release/v1alpha1/release_types.go
+++ pkg/apis/release/v1alpha1/release_types.go
@@ -135,9 +135,9 @@ spec:
 type ReleaseState string
 
 var (
-	stateActive     ReleaseState = "active"     // nolint:unused
-	stateDeprecated ReleaseState = "deprecated" // nolint:unused
-	stateWIP        ReleaseState = "wip"        // nolint:unused
+	stateActive     ReleaseState = "active"     // nolint
+	stateDeprecated ReleaseState = "deprecated" // nolint
+	stateWIP        ReleaseState = "wip"        // nolint
 	releaseCRD      *apiextensionsv1beta1.CustomResourceDefinition
 )
 
