02ebe543e8724f8cc7cda1c53ee06c6c26516408
Jose Armesto <github@armesto.net>
2020-04-30 16:10:06 +0200

    Linting

--- pkg/apis/core/v1alpha1/azure_cluster_types.go
+++ pkg/apis/core/v1alpha1/azure_cluster_types.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 import (
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/apiextensions/pkg/crd"
