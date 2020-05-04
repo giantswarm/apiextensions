b5dba7da7f3b850f63509c971a19a07cae845675
Marian Steinbach <marian@giantswarm.io>
2020-05-04 11:00:24 +0200

    Update generated files

--- pkg/clientset/versioned/fake/register.go
+++ pkg/clientset/versioned/fake/register.go
@@ -36,7 +36,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
-var parameterCodec = runtime.NewParameterCodec(scheme) // nolint
+var parameterCodec = runtime.NewParameterCodec(scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
 	applicationv1alpha1.AddToScheme,
 	backupv1alpha1.AddToScheme,
