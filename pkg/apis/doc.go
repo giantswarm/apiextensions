9fbd54791cea413ac00677c90013de156b85fb7c
Thomas Fussell <thomas@giantswarm.io>
2020-05-11 14:42:48 -0400

    Apply suggestions from code review
    Co-authored-by: Paweł Kopiczko <pawel@giantswarm.io>

--- pkg/apis/doc.go
+++ pkg/apis/doc.go
@@ -1,6 +1,6 @@
 /*
 
-apis contains definitions for all Giant Swarm custom resources organized by group and version.
+Package apis contains definitions for all Giant Swarm custom resources organized by group and version.
 As an example, pkg/apis/application/v1alpha1/app_catalog_types.go defines the custom resource accessible in a Kubernetes
 cluster as appcatalogs.application.giantswarm.io/v1alpha1 as well as the v1alpha1 section of the CRD named
 appcatalogs.application.giantswarm.io.
--- pkg/clientset/doc.go
+++ pkg/clientset/doc.go
@@ -1,6 +1,6 @@
 /*
 
-clientset is a generated set of clients providing CRUD interfaces for all Giant Swarm custom resources.
+Package clientset is a generated set of clients providing CRUD interfaces for all Giant Swarm custom resources.
 
 */
 package clientset
