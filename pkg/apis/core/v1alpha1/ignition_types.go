08f52c9bee011ab3588faf1301cec40dfc088df2
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2020-05-14 11:36:04 +0200

    Import sorting

--- pkg/apis/core/v1alpha1/ignition_types.go
+++ pkg/apis/core/v1alpha1/ignition_types.go
@@ -1,9 +1,10 @@
 package v1alpha1
 
 import (
-	"github.com/giantswarm/apiextensions/pkg/crd"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/apiextensions/pkg/crd"
 )
 
 const (
