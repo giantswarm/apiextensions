f0f4653e330f069d53dc2f48d46925e81b18e8af
wtty-fool <wtty.fool@gmail.com>
2020-05-28 17:47:26 +0200

    add organization CRD

---
+++ pkg/apis/security/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// +k8s:deepcopy-gen=package,register
+
+// +groupName=security.giantswarm.io
+package v1alpha1
---
+++ pkg/apis/security/v1alpha1/organization_types.go
@@ -0,0 +1,39 @@
+package v1alpha1
+
+import (
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/apiextensions/pkg/crd"
+)
+
+const (
+	kindOrganization = "Organization"
+)
+
+func NewOrganizationCRD() *v1beta1.CustomResourceDefinition {
+	return crd.LoadV1Beta1(group, kindOrganization)
+}
+
+// +genclient
+// +genclient:noStatus
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories=common;giantswarm
+// +kubebuilder:storageversion
+
+type Organization struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              OrganizationSpec `json:"spec"`
+}
+
+type OrganizationSpec struct {
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type OrganizationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []Organization `json:"items"`
+}
