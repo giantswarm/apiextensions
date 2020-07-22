cf8b0f8a25dd5309387b1238743cfa45da81b472
Nikola Prokopić <nikola@prokopic.rs>
2020-07-22 17:46:04 +0200

    Add node pool name annotation

---
+++ pkg/annotation/nodepool.go
@@ -0,0 +1,7 @@
+package annotation
+
+const (
+	// MachinePoolName is the annotation where customer-set human-friendly node
+	// pool name is stored.
+	MachinePoolName = "machine-pool.giantswarm.io/name"
+)
