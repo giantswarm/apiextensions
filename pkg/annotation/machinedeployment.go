6892e4a142c027da6a0453f6f071ac14039adcac
Marian Steinbach <marian@giantswarm.io>
2020-07-22 17:38:33 +0200

    Add annotations

---
+++ pkg/annotation/machinedeployment.go
@@ -0,0 +1,6 @@
+package annotation
+
+// MachineDeploymentSubnet is used to specify the IPv4 CIDR used for a
+// node pool defined by a MachineDeployment or AWSMachineDeployment
+// resource.
+const MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
