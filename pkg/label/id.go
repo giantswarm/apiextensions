9b8ed00310406471734fa79c105ee29b8e20f6b5
Nikola Prokopić <nikola@prokopic.rs>
2020-07-22 17:57:42 +0200

    Add node pool labels

--- pkg/label/id.go
+++ pkg/label/id.go
@@ -12,6 +12,11 @@ const ControlPlane = "giantswarm.io/control-plane"
 // identify which Worker Nodes the given CR groups together.
 const MachineDeployment = "giantswarm.io/machine-deployment"
 
+// MachinePool is the ID label put into all MachinePool and AzureMachinePool CRs
+// and it contains tenant cluster node pool ID (i.e. the machine pool ID). E.g.
+// giantswarm.io/machine-pool=de19f-h94vd.
+const MachinePool = "giantswarm.io/machine-pool"
+
 // Organization is the ID label put into all CRs to identify which Organization
 // the given CR is related to.
 const Organization = "giantswarm.io/organization"
