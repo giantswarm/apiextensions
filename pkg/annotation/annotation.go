375ba64b562256720036d76b9f092cf65ee6f59b
Marian Steinbach <marian@giantswarm.io>
2020-07-22 14:14:34 +0200

    Add ReleaseNotes annotation

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -4,3 +4,7 @@ package annotation
 // documentation. This aims to help understanding all the moving parts within
 // the system and how they relate to each other.
 const Docs = "giantswarm.io/docs"
+
+// ReleaseNotes is the annotation key for a URL to release notes, to learn
+// more about details of a release.
+const ReleaseNotes = "giantswarm.io/release-notes"
