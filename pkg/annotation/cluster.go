e669d3aaffd7cf48f41dc4bb436b6ae34b536e89
Adrian Barsan <mnlk123@yahoo.com>
2020-07-28 17:33:30 +0200

    Add cluster description annotation (#542)

---
+++ pkg/annotation/cluster.go
@@ -0,0 +1,5 @@
+package annotation
+
+// ClusterDescription is the cluster annotation used for storing
+// a customer's cluster description.
+const ClusterDescription = "cluster.giantswarm.io/description"
