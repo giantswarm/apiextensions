c27840cb3a27202021b8d7937f40f7e2291129f9
Jose Armesto <github@armesto.net>
2020-09-17 12:17:24 +0200

    Add annotations used for OIDC

---
+++ pkg/annotation/oidc.go
@@ -0,0 +1,7 @@
+package annotation
+
+// Annotations used to configure OIDC in Tenant Clusters.
+const OIDCClientID = "oidc.giantswarm.io/client-id"
+const OIDCIssuerURL = "oidc.giantswarm.io/issuer-url"
+const OIDCUsernameClaim = "oidc.giantswarm.io/username-claim"
+const OIDCGroupClaim = "oidc.giantswarm.io/group-claim"
