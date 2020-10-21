23dbba90c8e3f5b725a079260eca082321878028
Nikola Prokopić <nikola@prokopic.rs>
2020-10-21 15:56:52 +0200

    Add ProviderInfrastructureReady and NodePoolsReady condition types

--- pkg/conditions/clusterapi.go
+++ pkg/conditions/clusterapi.go
@@ -12,4 +12,13 @@ const (
 	// UpgradingCondition tells if the cluster is being upgraded to a newer
 	// release version.
 	UpgradingCondition capi.ConditionType = "Upgrading"
+
+	// ProviderInfrastructureReadyCondition tells if the provider
+	// infrastructure defined by the related provider-specific CR is Ready. For
+	// example, in Cluster CR it tells if Azure infrastructure defined by the
+	// AzureCluster is ready.
+	ProviderInfrastructureReadyCondition capi.ConditionType = "ProviderInfrastructureReady"
+
+	// NodePoolsReadyCondition tells if all node pools are ready.
+	NodePoolsReadyCondition capi.ConditionType = "NodePoolsReady"
 )
