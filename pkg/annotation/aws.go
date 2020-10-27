a6b0fcf5bccadf700f11ef894aff2367ff2a4663
calvix <vaclav@giantswarm.io>
2020-10-27 17:01:59 +0100

    improve docs

--- pkg/annotation/aws.go
+++ pkg/annotation/aws.go
@@ -2,8 +2,13 @@ package annotation
 
 // AWSUpdateMaxBatchSize is the aws update annotation used for configuring
 // maximum batch size for instances during ASG update.
+// The value can be either a whole number specifying the number of instances
+// or a percentage of total instances as decimal number ie: `0.30` for 30%.
+// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-maxbatchsize
 const AWSUpdateMaxBatchSize = "aws.giantswarm.io/update-max-batch-size"
 
 // AWSUpdatePauseTime is the aws update annotation used for configuring
 // time pause between rolling a single batch during ASG update.
+// The value must be in ISO_8601 format.
+// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-pausetime
 const AWSUpdatePauseTime = "aws.giantswarm.io/update-pause-time"
