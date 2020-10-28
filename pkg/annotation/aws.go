d97698f7f6aef25efc51872e590b49ebffc900ba
calvix <vaclav@giantswarm.io>
2020-10-28 09:04:08 +0100

    examples

--- pkg/annotation/aws.go
+++ pkg/annotation/aws.go
@@ -9,6 +9,6 @@ const AWSUpdateMaxBatchSize = "aws.giantswarm.io/update-max-batch-size"
 
 // AWSUpdatePauseTime is the aws update annotation used for configuring
 // time pause between rolling a single batch during ASG update.
-// The value must be in ISO 8601 duration format, e. g. "PT5M" for five minutes.
+// The value must be in ISO 8601 duration format, e. g. "PT5M" for five minutes or "PT10S" for 10 seconds.
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-pausetime
 const AWSUpdatePauseTime = "aws.giantswarm.io/update-pause-time"
