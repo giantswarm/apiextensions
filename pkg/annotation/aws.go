678c06ca869797040f52c24d990d83ff335c84fb
paurosello <pau@giantswarm.io>
2020-10-30 11:06:38 +0000

    Add subnet and metadata annotations

--- pkg/annotation/aws.go
+++ pkg/annotation/aws.go
@@ -12,3 +12,14 @@ const AWSUpdateMaxBatchSize = "alpha.aws.giantswarm.io/update-max-batch-size"
 // The value must be in ISO 8601 duration format, e. g. "PT5M" for five minutes or "PT10S" for 10 seconds.
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-pausetime
 const AWSUpdatePauseTime = "alpha.aws.giantswarm.io/update-pause-time"
+
+// AWSMetadataV2 is the aws update annotation used for configuring
+// the state of token usage for your instance metadata requests.
+// The value can be either optional or required.
+// https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-launchtemplate-launchtemplatedata-metadataoptions.html#cfn-ec2-launchtemplate-launchtemplatedata-metadataoptions-httpendpoint
+const AWSMetadataV2 = "alpha.aws.giantswarm.io/metadata-v2"
+
+// AWSSubnetSize is the aws update annotation used for configuring
+// the subnet size of AWSCluster or AWSMachineDeployment.
+// The value is a number that will represent the subnet mask used when creating the subnet. This value must be smaller than 28 due to AWS restrictions.
+const AWSSubnetSize = "alpha.aws.giantswarm.io/aws-subnet-size"
