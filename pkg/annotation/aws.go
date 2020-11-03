e197fe8a6d399b52abbc6d6e23d9563aeff5eebd
paurosello <pau@giantswarm.io>
2020-11-03 09:45:59 +0100

    Update pkg/annotation/aws.go
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>

--- pkg/annotation/aws.go
+++ pkg/annotation/aws.go
@@ -13,8 +13,15 @@ const AWSUpdateMaxBatchSize = "alpha.aws.giantswarm.io/update-max-batch-size"
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-pausetime
 const AWSUpdatePauseTime = "alpha.aws.giantswarm.io/update-pause-time"
 
-// AWSMetadataV2 is the aws update annotation used for configuring
-// the state of token usage for your instance metadata requests.
+// AWSMetadataV2 configures token usage for your AWS EC2 instance metadata requests.
+// If the value is 'optional', you can choose to retrieve instance metadata with or without a signed token
+// header on your request. If you retrieve the IAM role credentials without a token, the version 1.0 role
+// credentials are returned. If you retrieve the IAM role credentials using a valid signed token, the version
+// 2.0 role credentials are returned.
+// If the state is 'required', you must send a signed token header with any instance metadata retrieval
+// requests. In this state, retrieving the IAM role credentials always returns the version 2.0 credentials; the
+// version 1.0 credentials are not available.
+// Default: 'optional'
 // The value can be either "optional" or "required". Default is "optional".
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-launchtemplate-launchtemplatedata-metadataoptions.html#cfn-ec2-launchtemplate-launchtemplatedata-metadataoptions-httptokens
 const AWSMetadataV2 = "alpha.aws.giantswarm.io/metadata-v2"
