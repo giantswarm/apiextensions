b95692b9ce09dd8b6ce269509bec629fa111cdc7
Jihyuk Bok <jhbok@giantswarm.io>
2020-12-03 11:40:43 +0100

    add annotations for apps

---
+++ pkg/annotation/app.go
@@ -0,0 +1,9 @@
+package annotation
+
+// LatestConfigMapVersion is the highest resource version among the configmaps
+// app CRs depends on.
+const LatestConfigMapVersion = "app-operator.giantswarm.io/giantswarm.io/latest-configmap-version"
+
+// LatestSecretVersion is the highest resource version among the secret
+// app CRs depends on.
+const LatestSecretVersion = "app-operator.giantswarm.io/latest-secret-version"
