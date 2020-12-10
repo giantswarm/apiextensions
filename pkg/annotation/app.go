d04c506468281a01c7729d82d89ce4fc6820b00f
kubasobon <wtty.fool@gmail.com>
2020-12-10 12:30:52 +0100

    add pause-reconcilliation annotation

--- pkg/annotation/app.go
+++ pkg/annotation/app.go
@@ -7,3 +7,7 @@ const LatestConfigMapVersion = "app-operator.giantswarm.io/giantswarm.io/latest-
 // LatestSecretVersion is the highest resource version among the secret
 // app CRs depends on.
 const LatestSecretVersion = "app-operator.giantswarm.io/latest-secret-version"
+
+// PauseReconcilliation annotation stops app-operator from reconciling App CR
+// using it.
+const PauseReconcilliation = "app-operator.giantswarm.io/pause"
