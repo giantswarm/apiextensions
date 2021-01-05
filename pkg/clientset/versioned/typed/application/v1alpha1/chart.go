519bdb50f01c0f1b0cb90b531bc1d1fb00aa31e1
Marian Steinbach <marian@giantswarm.io>
2021-01-05 16:39:52 +0100

    Update generated stuff

--- pkg/clientset/versioned/typed/application/v1alpha1/chart.go
+++ pkg/clientset/versioned/typed/application/v1alpha1/chart.go
@@ -22,13 +22,12 @@ import (
 	"context"
 	"time"
 
+	v1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
+	scheme "github.com/giantswarm/apiextensions/v3/pkg/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	rest "k8s.io/client-go/rest"
-
-	v1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
-	scheme "github.com/giantswarm/apiextensions/v3/pkg/clientset/versioned/scheme"
 )
 
 // ChartsGetter has a method to return a ChartInterface.
