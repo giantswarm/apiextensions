d262c88255be07d92771155f4618f599875103d7
Pau Rosello <paurosello@gmail.com>
2021-02-01 18:18:36 +0100

    fix yaml

--- pkg/annotation/aws.go
+++ pkg/annotation/aws.go
@@ -1,25 +1,25 @@
 package annotation
 
-// Support:
+// support:
 // - infrastructure.giantswarm.io/v1alpha2/AWSCluster
-// Documentation:
+// documentation:
 // AWSCNIMinimumIPTarget is an annotation to configure the value for MINIMUM_IP_TARGET for AWS CNI
 // see https://github.com/aws/amazon-vpc-cni-k8s#cni-configuration-variables
 // and https://github.com/aws/amazon-vpc-cni-k8s/blob/master/docs/eni-and-ip-target.md
 const AWSCNIMinimumIPTarget = "alpha.cni.aws.giantswarm.io/minimum-ip-target"
 
-// Support:
+// support:
 // - infrastructure.giantswarm.io/v1alpha2/AWSCluster
-// Documentation:
+// documentation:
 // AWSCNIWarmIPTarget is an annotation to configure the value for WARM_IP_TARGET for AWS CNI
 // see https://github.com/aws/amazon-vpc-cni-k8s#cni-configuration-variables
 // and https://github.com/aws/amazon-vpc-cni-k8s/blob/master/docs/eni-and-ip-target.md
 const AWSCNIWarmIPTarget = "alpha.cni.aws.giantswarm.io/warm-ip-target"
 
-// Support:
+// support:
 // - infrastructure.giantswarm.io/v1alpha2/AWSCluster
 // - infrastructure.giantswarm.io/v1alpha2/AWSMachineDeployment
-// Documentation:
+// documentation:
 // AWSUpdateMaxBatchSize is the aws update annotation used for configuring
 // maximum batch size for instances during ASG update.
 // The value can be either a whole number specifying the number of instances
@@ -27,20 +27,20 @@ const AWSCNIWarmIPTarget = "alpha.cni.aws.giantswarm.io/warm-ip-target"
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-maxbatchsize
 const AWSUpdateMaxBatchSize = "alpha.aws.giantswarm.io/update-max-batch-size"
 
-// Support:
+// support:
 // - infrastructure.giantswarm.io/v1alpha2/AWSCluster
 // - infrastructure.giantswarm.io/v1alpha2/AWSMachineDeployment
-// Documentation:
+// documentation:
 // AWSUpdatePauseTime is the aws update annotation used for configuring
 // time pause between rolling a single batch during ASG update.
 // The value must be in ISO 8601 duration format, e. g. "PT5M" for five minutes or "PT10S" for 10 seconds.
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatepolicy.html#cfn-attributes-updatepolicy-rollingupdate-pausetime
 const AWSUpdatePauseTime = "alpha.aws.giantswarm.io/update-pause-time"
 
-// Support:
+// support:
 // - infrastructure.giantswarm.io/v1alpha2/AWSCluster
 // - infrastructure.giantswarm.io/v1alpha2/AWSMachineDeployment
-// Documentation:
+// documentation:
 // AWSMetadataV2 configures token usage for your AWS EC2 instance metadata requests.
 // If the value is 'optional', you can choose to retrieve instance metadata with or without a signed token
 // header on your request. If you retrieve the IAM role credentials without a token, the version 1.0 role
@@ -53,10 +53,10 @@ const AWSUpdatePauseTime = "alpha.aws.giantswarm.io/update-pause-time"
 // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-launchtemplate-launchtemplatedata-metadataoptions.html#cfn-ec2-launchtemplate-launchtemplatedata-metadataoptions-httptokens
 const AWSMetadataV2 = "alpha.aws.giantswarm.io/metadata-v2"
 
-// Support:
+// support:
 // - infrastructure.giantswarm.io/v1alpha2/AWSCluster
 // - infrastructure.giantswarm.io/v1alpha2/AWSMachineDeployment
-// Documentation:
+// documentation:
 // AWSSubnetSize is the aws update annotation used for configuring
 // the subnet size of AWSCluster or AWSMachineDeployment.
 // The value is a number that will represent the subnet mask used when creating the subnet. This value must be smaller than 28 due to AWS restrictions.
