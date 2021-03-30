463fd51ea202cf3d63d8fbd3c8e018a571719b12
Oliver Ponder <oliver.ponder@gmail.com>
2021-03-30 19:25:24 +0800

    Document original-organization-name annotation

---
+++ pkg/annotation/organization.go
@@ -0,0 +1,9 @@
+package annotation
+
+// support:
+//   - crd: organization.security.giantswarm.io
+//     apiversion: v1alpha1
+// documentation:
+//   This annotation contains the original name of organizations migrated from
+//   our legacy organization management service.
+const UIOrganizationOriginalName = "ui.giantswarm.io/original-organization-name"
