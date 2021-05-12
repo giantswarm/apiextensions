2afef000848b4fa277f64417d910a729e0506f21
Jose Armesto <github@armesto.net>
2021-05-12 12:55:40 +0200

    Add status to the Organization CRD (#726)
    * Add status to the Organization CRD
    
    * Make namespace field optional

--- pkg/apis/security/v1alpha1/organization_types.go
+++ pkg/apis/security/v1alpha1/organization_types.go
@@ -10,16 +10,26 @@ import (
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:resource:scope=Cluster,categories=common;giantswarm,shortName=org;orgs
 // +kubebuilder:storageversion
+// +kubebuilder:subresource:status
 // +k8s:openapi-gen=true
 // Organization represents schema for managed Kubernetes namespace. Reconciled by organization-operator.
 type Organization struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
 	Spec              OrganizationSpec `json:"spec"`
+	// +kubebuilder:validation:Optional
+	Status OrganizationStatus `json:"status,omitempty"`
 }
 
 // +k8s:openapi-gen=true
 type OrganizationSpec struct {
+}
+
+// +k8s:openapi-gen=true
+type OrganizationStatus struct {
+	// +kubebuilder:validation:Optional
+	// Namespace is the namespace containing the resources for this organization.
+	Namespace string `json:"namespace,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
