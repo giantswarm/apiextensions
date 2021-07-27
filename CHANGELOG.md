f468d05744a34c706acc4b00523475012734ba85
Nick Jüttner <nick@giantswarm.io>
2021-07-27 09:45:20 +0200

    GS AWS v1alpha3 CRDs (#760)
    * GS AWS v1alpha3 CRDs
    
    * GS AWS v1alpha3 CRDs
    
    * GS AWS v1alpha3 CRDs
    
    * GS AWS v1alpha3 CRDs
    
    * Update CHANGELOG.md
    
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>
    
    * Apply suggestions from code review
    
    * v1alpha3
    
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add `v1alpha3` version for Giant Swarm AWS CRDs.
+
 ### Changed
 
 - Add documentation for the silence.monitoring.giantswarm.io/v1alpha1 CRD.
--- pkg/apis/infrastructure/v1alpha3/network_pool_types.go
+++ pkg/apis/infrastructure/v1alpha3/network_pool_types.go
@@ -1,4 +1,4 @@
-package v1alpha2
+package v1alpha3
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -6,6 +6,7 @@ import (
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:storageversion
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:categories=giantswarm;cluster-api
 // +k8s:openapi-gen=true
