6fa1f21c1143c21dd5d5068f08f5f2034052fa11
Jose Armesto <github@armesto.net>
2022-03-17 11:44:43 +0100

    Don't ensure CAPZ crds on azure

--- hack/assets.go
+++ hack/assets.go
@@ -4,23 +4,7 @@ import (
 	"github.com/giantswarm/apiextensions/v5/pkg/crd"
 )
 
-var upstreamReleaseAssets = []crd.ReleaseAssetFileDefinition{
-	// azure
-	{
-		Owner:    "kubernetes-sigs",
-		Repo:     "cluster-api-provider-azure",
-		Version:  "v1.1.0",
-		Files:    []string{"infrastructure-components.yaml"},
-		Provider: "azure",
-	},
-	{
-		Owner:    "Azure",
-		Repo:     "aad-pod-identity",
-		Version:  "v1.8.0",
-		Files:    []string{"deployment.yaml"},
-		Provider: "azure",
-	},
-}
+var upstreamReleaseAssets = []crd.ReleaseAssetFileDefinition{}
 
 var remoteRepositories = []crd.RemoteRepositoryDefinition{
 	{
