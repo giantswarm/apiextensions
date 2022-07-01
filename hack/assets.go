5d35394954a84475b2193734a374753a9a7651e3
Laszlo Uveges <laszlo@giantswarm.io>
2022-07-01 15:37:20 +0200

    Bump apiextensions-applications to v0.5.0 and run make generate

--- hack/assets.go
+++ hack/assets.go
@@ -12,7 +12,7 @@ var remoteRepositories = []crd.RemoteRepositoryDefinition{
 		Owner:     "giantswarm",
 		Provider:  "common",
 		Name:      "apiextensions-application",
-		Reference: "v0.4.1",
+		Reference: "v0.5.0",
 	},
 	{
 		Path:      "config/crd",
