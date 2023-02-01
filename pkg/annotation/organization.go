5ebf67a15762f9f1b476a06b13a247447dfe8603
Christian Bianchi <whites11@gmail.com>
2023-02-01 11:53:11 +0100

    make generate

--- pkg/annotation/organization.go
+++ pkg/annotation/organization.go
@@ -3,7 +3,9 @@ package annotation
 // support:
 //   - crd: organization.security.giantswarm.io
 //     apiversion: v1alpha1
+//
 // documentation:
-//   This annotation contains the original name of organizations migrated from
-//   our legacy organization management service.
+//
+//	This annotation contains the original name of organizations migrated from
+//	our legacy organization management service.
 const UIOriginalOrganizationName = "ui.giantswarm.io/original-organization-name"
